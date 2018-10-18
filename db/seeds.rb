82f21eda65743889647fc4f8f4ec0484118b98e6
jamestouri <j_touri@yahoo.com>
2018-10-18 14:30:40 -0700

    changing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(email: 'james@james.com', first_name: 'james', last_name: 'james', password: '123456')
+user1 = User.create(email: 'aaron@aaron.com', first_name: 'aaron', last_name: 'sis', password: '123456')
 user2 = User.create(email: 'tim@tim.com', first_name: 'Tim', last_name: 'Stapes', password: '123456')
 user3 = User.create(email: 'nick@nick.com', first_name: 'Nick', last_name: 'Cas', password: '123456')
 user4 = User.create(email: 'oli@oli.com', first_name: 'Oli', last_name: 'Pierce', password: '123456')
